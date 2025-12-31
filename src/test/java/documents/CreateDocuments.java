94cea2df76a4fdf137cca3d5d3c8b9c737b5afef
aship <aship@DESKTOP-E31OS9C>
2025-12-31 07:35:48 +0530

    doc updated

--- src/test/java/documents/CreateDocuments.java
+++ src/test/java/documents/CreateDocuments.java
@@ -1,8 +1,11 @@
 package documents;
 
+import org.testng.annotations.Test;
+
 public class CreateDocuments {
 
-	public static void main(String[] args) {
+	@Test
+	public void m1(){
 		System.out.println("---documents  created");
 	}
 }
